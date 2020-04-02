ac0f899c7a9b0b742db26b4a294965705945aa9a
Dmytro Rudakov <ext-dmytro.rudakov@here.com>
2020-04-02 13:10:52 +0300

    Add bunch data upload; Add prettier config; Update yarn.lock
    Signed-off-by: Dmytro Rudakov <ext-dmytro.rudakov@here.com>

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,52 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const Tour = require('../../models/tourModel');
+
+dotenv.config();
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('DB connection successful'));
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
+);
+
+//IMPORT DATA INTO DATABASE
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('Data successfully loaded');
+  } catch (err) {
+    console.log('err: ', err);
+  }
+};
+
+//DELETE AL DATA FROM COLLECTION
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('Data successfully deleted');
+    process.exit();
+  } catch (err) {
+    console.log('err: ', err);
+  }
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
+
+console.log(process.argv);
