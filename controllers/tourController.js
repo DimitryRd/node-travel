e52ce190d156f588eefaa25f85e1ad5c753e444c
Dmytro Rudakov <ext-dmytro.rudakov@here.com>
2020-04-08 10:51:36 +0300

    Add pagination

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -54,6 +54,19 @@ exports.getAllTours = async (req, res) => {
       query = query.select('-__v');
     }
 
+    // 5) Pagination
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 100;
+    const skip = (page - 1) * limit;
+
+    // page=2&limit=10 1-10, page 1, 11-20, page 2, 21-30 page3
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page does not exist');
+    }
+
     // EXECUTE QUERY
     const tours = await query;
 
