04981e7a496f990eb77afddbe8ec7a745eab7bc3
Dmytro Rudakov <ext-dmytro.rudakov@here.com>
2020-05-06 16:59:05 +0300

    Add aggreation middleware
    Signed-off-by: Dmytro Rudakov <ext-dmytro.rudakov@here.com>

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -108,9 +108,6 @@ exports.getTourStats = async (req, res) => {
       },
       {
         $sort: { avgPrice: 1 }
-      },
-      {
-        $match: { _id: { $ne: 'EASY' } }
       }
     ]);
     res.status(200).json({ status: 'OK', data: { stats } });
--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -7,6 +8,7 @@ const tourSchema = new mongoose.Schema(
       required: [true, 'A tour must have a name'],
       unique: true
     },
+    slug: { type: String },
     rating: {
       type: Number,
       default: 4.5
@@ -45,7 +47,8 @@ const tourSchema = new mongoose.Schema(
     imageCover: { type: String, required: [true, 'A tour must have an image'] },
     images: [String],
     createdAt: { type: String, default: Date.now(), select: false },
-    startDates: [Date]
+    startDates: [Date],
+    secretTour: { type: Boolean, default: false }
   },
   {
     toJSON: { virtuals: true },
@@ -55,6 +58,42 @@ const tourSchema = new mongoose.Schema(
 
 tourSchema.virtual('durationWeeks').get(function() {
   return this.duration / 7;
+});
+
+//DOCUMENT MIDDLEWARE: runs before .save() and .create
+tourSchema.pre('save', function(next) {
+  console.log(this);
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+tourSchema.pre('save', next => {
+  console.log('Will save document');
+  next();
+});
+
+tourSchema.post('save', function(doc, next) {
+  console.log(doc);
+  next();
+});
+
+//QUERY MIDDLEWARES
+tourSchema.pre(/^find/, function(next) {
+  this.find({ secretTour: { $ne: false } });
+  this.start = Date.now();
+  next();
+});
+
+tourSchema.post(/^find/, function(docs, next) {
+  console.log(`Quest took ${Date.now() - this.start} milliseconds`);
+  next();
+});
+
+//AGGREGATION MIDDLEWARE
+tourSchema.pre('aggregate', function(next) {
+  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+  console.log('pipeline: ', this.pipeline());
+  next();
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
