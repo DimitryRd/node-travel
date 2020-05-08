60ce39613488c0ec0746f216a297cf8f3859bf2e
Dmytro Rudakov <ext-dmytro.rudakov@here.com>
2020-05-08 21:53:17 +0300

    Add global error handlers
    Signed-off-by: Dmytro Rudakov <ext-dmytro.rudakov@here.com>

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
+const AppError = require('./utils/appError.js');
 
 const userRoutes = require('./routers/userRoutes');
 const tourRoutes = require('./routers/tourRoutes');
+const globalErrorHandler = require('./controllers/errorController.js');
 
 const app = express();
 
@@ -15,11 +17,6 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/starter/public`));
 
 app.use((req, res, next) => {
-  console.log('Hello from the middleware!  ✋🏻✋🏻✋🏻  ');
-  next();
-});
-
-app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
@@ -27,5 +24,20 @@ app.use((req, res, next) => {
 // 3) ROUTES
 app.use('/api/v1/tours', tourRoutes);
 app.use('/api/v1/tours', userRoutes);
+
+app.all('*', (req, res, next) => {
+  // res.status(404).json({
+  //   status: 'Fail',
+  //   message: `Cant find ${req.originalUrl} on this server`
+  // });
+
+  // const err = new Error('Fucking error!');
+  // err.status = 'Fail';
+  // err.statusCode = 404;
+
+  next(new AppError(`Cant find ${req.originalUrl} on this server`, 404));
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
